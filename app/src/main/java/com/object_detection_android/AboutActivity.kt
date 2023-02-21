b61aa7506ebab6c33d87215327af753a861da951
mahdisahraei <mahdi_sahrai@yahoo.com>
2023-02-21 15:19:40 +0330

    the section of about-app was added

--- app/src/main/java/com/object_detection_android/AboutActivity.kt
+++ app/src/main/java/com/object_detection_android/AboutActivity.kt
@@ -1,8 +1,10 @@
 package com.object_detection_android
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.object_detection_android.databinding.ActivityAboutBinding
 
 
@@ -16,8 +18,35 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        clicking()
     }
 
+
+    fun clicking() {
+
+        binding.github.setOnClickListener(View.OnClickListener {
+            val url = "https://github.com/mehdisahraeei/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        })
+
+        binding.instagram.setOnClickListener(View.OnClickListener {
+            val url = "https://www.instagram.com/_mehdi_sahraei/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        })
+
+        binding.kaggle.setOnClickListener(View.OnClickListener {
+            val url = "https://www.kaggle.com/datasets/mehdisahraei/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        })
+
+    }
 
     override fun onBackPressed() {
         startActivity(Intent(this@AboutActivity, MainActivity::class.java))
